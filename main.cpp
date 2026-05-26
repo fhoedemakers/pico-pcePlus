74033ffd82cd1aaa5af9e66efd278e3455eff429
Frank Hoedemakers <frank.hoedemakers@gmail.com>
2026-05-26 18:19:30 +0200

    screenmode is set correctly when menu exits.

--- main.cpp
+++ main.cpp
@@ -806,7 +806,6 @@ int main()
             }
             printf("Starting game\n");
             Frens::PaceFrames60fps(true);
-             scaleMode8_7_ = Frens::applyScreenMode(settings.screenMode);
             process();
             if (isCDGame) {
                 // Close BIN handle (still-open until cd_term runs). The BIOS
